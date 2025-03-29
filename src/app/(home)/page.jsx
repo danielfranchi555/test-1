bd387afa1a6abe2774f22661a0ad4c9a72928ffb
danielfranchi555 <danielfranchi3@gmail.com>
2025-03-29 04:58:28 -0300

    error-prisma

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ButtonLogut from '@/components/ButtonLogut';
-
+import { getUsers } from '@/app/actions/auth/users';
 export default async function page() {
   const { data: users, error } = await getUsers();
   if (error) {
